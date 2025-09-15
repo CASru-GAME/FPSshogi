8a3d9d995488f3beea31144d08fa558700159d2c
野田龍生 <20061216ryu.no@gmail.com>
2025-09-15 22:23:22 +0900

    Exitbuttonのコード
    コードを保存した

--- Assets/App/Scripts/Title/ExitButton/ExitGame.cs
+++ Assets/App/Scripts/Title/ExitButton/ExitGame.cs
@@ -1,6 +1,33 @@
 using UnityEngine;
 
-public class ExitGame
+public class ExitGame : MonoBehaviour
 {
-    
+    public GameObject confirmPanel; // 確認パネルを割り当て
+
+    void Start() {
+        if (confirmPanel != null) {
+            confirmPanel.SetActive(false);
+        }
+    }
+
+    // Exitボタンを押したときに呼ぶ
+    public void ShowConfirmPanel() {
+        if (confirmPanel != null) {
+            confirmPanel.SetActive(true);
+        }
+    }
+
+    // Yesボタン
+    public void QuitGame() {
+        Debug.Log("ゲーム終了");
+        Application.Quit();
+    }
+
+    // Noボタン
+    public void CancelExit() {
+        if (confirmPanel != null) {
+            confirmPanel.SetActive(false);
+        }
+    }
 }
+
